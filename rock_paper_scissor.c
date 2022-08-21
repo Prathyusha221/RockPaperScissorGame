efc0fda11c8c6a14061a0c5ee32c4b319a2b0c84
Prathyusha221 <65334227+Prathyusha221@users.noreply.github.com>
2022-08-21 21:00:37 +0530

    Update rock_paper_scissor.c

--- rock_paper_scissor.c
+++ rock_paper_scissor.c
@@ -1,95 +1,94 @@
 #include<time.h>
 #include<stdio.h>
+#include<stdlib.h>
 #include<assert.h>
 #include<string.h>
 int main()
 {
 	srand(time(NULL));
-	char user_i[8];
-    char r[4]="rock",p[5]="paper",s[8]="scissor",q[4]="quit";
-	char arr[4]="prs";
-	char none[2]="";
-	char c;
+	char user_i[9];
+    char r[9]="rock",p[9]="paper",s[9]="scissor",q[9]="quit";
+	char arr[3][9]={"rock","paper","scissor"};
 	
 	while(1)
 	{
-        printf("please choose rock,paper,scissor or quit: ");
+		printf("please choose rock,paper,scissor or quit: ");
 	    scanf("%s", user_i);
-	    if(strcmp(user_i,none)==0)
-	    {
-	    	continue;
-		}
-	    if(strcmp(user_i,q)==0)
+
+		if(strcmp(user_i,q)==0)
 	    {
 	    	return 0;
 	    }
+
+	    if(!((strcmp(user_i,"rock")==0) || (strcmp(user_i,"paper")==0) || (strcmp(user_i,"scissor")==0)))
+	    {
+			printf("Invalid input\n");
+	    	continue;
+		}
 	    
 	    printf("\n\n");
 	    printf("You chose: %s\n", user_i);
 	    
-		c=arr[rand()%3];
+		char c[8];
+		strcpy(c,arr[rand()%3]);
 	    
-	    switch(c)
-	    {
-	    	case 'p': 
-	    	    printf("Computer chose: Paper\n");
-	    	    if(strcmp(user_i,p)==0)
-	    	    {
-	    	    	printf("Result: none won\n");
-	    	    	
-				}
-				 if(strcmp(user_i,r)==0)
-				 {
-				 	printf("Result: Computer won\n");
-				 	
-				 }
-				 if(strcmp(user_i,s)==0)
-				 {
-				 	printf("Result: Player won\n");
-				 	
-				 }
-				 break;
-			case 'r':
-			    printf("Computer chose: Rock\n");
-				if(strcmp(user_i,p)==0)
-	    	    {
-	    	    	printf("Result: Player won\n");
-	    	    	
-				}
-				 if(strcmp(user_i,r)==0)
-				 {
-				 	printf("Result: none won\n");
-				 	
-				 }
-				 if(strcmp(user_i,s)==0)
-				 {
-				 	printf("Result: Computer won\n");
-				 	
-				 }
-				 break;	 
-			case 's':
-			    printf("Computer chose: Scissor\n");
-				if(strcmp(user_i,p)==0)
-	    	    {
-	    	    	printf("Result: Computer won\n");
-	    	    	
-				}
-				 if(strcmp(user_i,r)==0)
-				 {
-				 	printf("Result: Player won\n");
-				 	
-				 }
-				 if(strcmp(user_i,s)==0)
-				 {
-				 	printf("Result: none won\n");
-				 	
-				 }
-				 break;	 	 
-			default: 
-			    printf("miracle\n");	 
+		if(c[0]=='p'){
+			printf("Computer chose: paper\n");
+	        if(strcmp(user_i,p)==0)
+	    	{
+	    	    printf("Result: none won\n");    	    	
+			}
+			else if(strcmp(user_i,r)==0)
+			{
+				printf("Result: Computer won\n");	 	
+			}
+			else if(strcmp(user_i,s)==0)
+			{
+			 	printf("Result: Player won\n");
+			}
+			else{
+				continue;
+			}
 		}
-	    
-		
+		else if(c[0]=='r'){
+			printf("Computer chose: rock\n");
+			if(strcmp(user_i,p)==0)
+    	    {
+	  	    	printf("Result: Player won\n");    	
+			}
+			else if(strcmp(user_i,r)==0)
+			{
+			 	printf("Result: none won\n");
+			}
+			else if(strcmp(user_i,s)==0)
+			{
+			 	printf("Result: Computer won\n"); 	
+			}
+			else{
+				continue;
+			}
+		}
+		else if(c[0]=='s'){
+			printf("Computer chose: scissor\n");
+			if(strcmp(user_i,p)==0)
+		    {
+	        	printf("Result: Computer won\n");
+			}
+			else if(strcmp(user_i,r)==0)
+			{
+			 	printf("Result: Player won\n");	
+			}
+			else if(strcmp(user_i,s)==0)
+			{
+			 	printf("Result: none won\n"); 	
+			}
+			else{
+				continue;
+			}
+		}
+	    else{
+			printf("miracle\n"); 
+		}
     }
 	return 0;
 }
